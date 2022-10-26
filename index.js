c4d1244fd01e4f299a3797ac80b8f32b52b305d4
Rodel Cabubas <rodel.cabubas@cebupacificair.com>
2022-10-26 17:36:26 +0800

    error handling

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+// middlewares
+app.use(express.json());
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode? res.statusCode:500;
+    res.status(statusCode);
+    res.json({
+        error: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        innerTargetException: process.env.NODE_ENV === 'production' ? null : err.innerTargetException
+    });
+}
+
 app.use("/api", require("./routes"));
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => console.log("Proxy Server running. Port: ", process.env.PORT));
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const asyncHandler = require("express-async-handler");
+
+const requestHandler = asyncHandler(async (req,res, next) => {
+  try {  
+    let pathSuffix = req.originalUrl.replace("/api", ""),
+        targetURL = process.env.TARGET_BASE_URL + pathSuffix;
+ 
+    console.log("target:", req.method);
+
+    const targetResponse = await axios({
+      method: req.method,
+      url: targetURL,
+      data: req.body
+    });
+
+    res.status(targetResponse.status).json(targetResponse.data);
+  } catch (error) {
+    console.log(error.response.data)
+    res.statusCode = error.response? error.response.status:null;
+    error.innerTargetException = error.response? error.response.data:null;
+    // throw new Error(error.message? error.message:error);
+    next(error);
+  }
+});
 
 // all get request route
 router.get("/*", requestHandler);
@@ -8,16 +32,5 @@ router.get("/*", requestHandler);
 // all post request route
 router.post("/*", requestHandler);
 
-
-const requestHandler = async (req,res) => {
-  const pathSuffix = req.originalUrl.replace("/api", ""),
-        targetURL = process.env.TARGET_BASE_URL + pathSuffix;
-        
-  console.log("target: ", targetURL);
-
-  const targetResponse = await axios.get(targetURL);
-
-  res.status(targetResponse.status).json(targetResponse.data);
-}
 
 module.exports = router;
